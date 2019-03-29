39a77cb3824b7ad5356fc42680d32d538ebb63f3
VENKY <VENKY@DESKTOP-ER90K33>
2019-03-29 12:09:50 +0530

    Sample Crm application

---
+++ com/Infoflo/Maps/NewTest.java
@@ -0,0 +1,9 @@
+package com.Infoflo.Maps;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+  }
+}
---
+++ src/test/java/com/Infoflo/Maps/Maps.java
@@ -0,0 +1,14 @@
+package com.Infoflo.Maps;
+
+import org.testng.annotations.Test;
+
+public class Maps {
+@Test
+public void maps()
+    {
+		String s="India Map";
+		System.out.println(s);
+		
+	}
+}
+
